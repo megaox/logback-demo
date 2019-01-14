1edffde3d45d430720d87e4827ac41e60c0559cf
megaox <megaox@163.com>
2019-01-15 02:03:42 +0800

    boot版本升级

--- src/main/java/com/geek/LogbackDemoApplication.java
+++ src/main/java/com/geek/LogbackDemoApplication.java
@@ -1,12 +1,18 @@
 package com.geek;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class LogbackDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogbackDemoApplication.class, args);
+		log.info("haha");
+		log.debug("haha");
+		log.warn("haha");
+		log.error("haha");
 	}
 }
